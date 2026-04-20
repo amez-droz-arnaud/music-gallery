d4169d4bc91f3bab80770430ed2dad885560eb98
Amez-Droz Arnaud <amezdrozarnaud@gmail.com>
2026-04-20 23:14:36 +0200

    vite.config base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  // Repo
-  base: '/music-gallery/',
+  // Repo /repo-name/
+  base: './',
 
   build: {
     outDir: 'dist',
